a490fd62feb56a0b1bebc860e245c0a49d45dc8e
petri.ryynanen <petri.ryynanen@CPX-O73OAILTNM9.dir.svc.accenture.com>
2019-01-07 15:47:36 +0200

    update

--- CucumberBDD/src/test/java/runners/Test_Background_Scenario_Runner.java
+++ CucumberBDD/src/test/java/runners/Test_Background_Scenario_Runner.java
@@ -13,7 +13,7 @@ import cucumber.api.junit.Cucumber;
 				plugin = {"pretty", 
 						 "html:target/cucumber-htmlreport",
 						 "json:target/cucumber-report1.json,",
-						 "com.cucumber.listener.ExtentCucumberFormatter:target/report1.html"})
+						 "com.cucumber.listener.ExtentCucumberFormatter:target/ExtentReport.html"})
 
 public class Test_Background_Scenario_Runner {
 
